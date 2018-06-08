fee4b758bad1bdcce9f16cf82cc5cc745d75eba2
mapan <mapan@NBJKTTCHD3T4.local>
2018-06-08 13:40:28 +0700

    variables example

---
+++ variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b int
+	var d, e int = 1, 2
+
+	fmt.Println(b)
+	fmt.Println(d, e)
+
+	var f = false
+	fmt.Println(f)
+
+	var g = "string g"
+	fmt.Println(g)
+
+	h := "string h"
+	fmt.Println(h)
+}
