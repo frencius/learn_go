fe49d1b4cc8748fcfb0d779a12a84ffb44858cde
mapan <mapan@NBJKTTCHD3T4.local>
2018-06-08 13:08:11 +0700

    first commit -  hello and values

---
+++ values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("7 + 3 = ", 7+3)
+	fmt.Println("7.3 / 3.7 = ", 7.3/3)
+	fmt.Println("7 % 3 = ", 7%3)
+	fmt.Println(true)
+	fmt.Println(true && false)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+}
