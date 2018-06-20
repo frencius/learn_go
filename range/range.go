cac210a49d71de8b9eaa4c57cbf1319e01f1e2c4
mapan <mapan@NBJKTTCHD3T4.local>
2018-06-20 17:13:09 +0700

    range

---
+++ range/range.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 3, 4}
+	fmt.Println("nums:", nums)
+
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"satu": "1", "dua": "2"}
+
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Printf("%s\n", k)
+	}
+
+	for _, v := range kvs {
+		fmt.Printf("%s\n", v)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
+}
